db6cfcd8ed1c8d66c17920890b4c37ea14b17c92
vlados096zver <81378975+vlados096zver@users.noreply.github.com>
2024-10-05 15:55:56 +0300

    init

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'notifications',
-  webDir: 'dist'
+  webDir: 'dist/notifications',
+  plugins: {
+    PushNotifications: {
+      presentationOptions: ["badge", "sound", "alert"],
+    },
+  },
 };
 
 export default config;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { PushNotifications, Token } from '@capacitor/push-notifications';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'notifications';
+export class AppComponent implements OnInit {
+ title = 'notifications';
+   ngOnInit() {
+    this.initializePushNotifications();
+  }
+
+  initializePushNotifications() {
+    // Запрос разрешений
+    PushNotifications.requestPermissions().then(permission => {
+      if (permission.receive === 'granted') {
+        // Регистрация устройства
+        PushNotifications.register();
+      }
+    });
+
+    // Получение токена для отправки сообщений на устройство
+    PushNotifications.addListener('registration', (token: Token) => {
+      console.log('Registered with token: ', token.value);
+    });
+
+    // Обработка ошибок
+    PushNotifications.addListener('registrationError', (error: any) => {
+      console.error('Registration error: ', error);
+    });
+
+    // Получение уведомлений
+    PushNotifications.addListener('pushNotificationReceived', (notification: any) => {
+      console.log('Push notification received: ', notification);
+      alert('Notification received in foreground: ' + JSON.stringify(notification));
+    });
+
+    // Обработка нажатий на уведомления
+    PushNotifications.addListener('pushNotificationActionPerformed', (notification: any) => {
+      console.log('Push notification action performed: ', notification);
+      alert('Notification action: ' + JSON.stringify(notification));
+    });
+  }
+  
 }
